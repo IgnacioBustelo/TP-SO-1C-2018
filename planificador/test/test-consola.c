a95f4652039d63589acd1943bd6b7c756d2bbbe0
Fernando <wzfernandochen@gmail.com>
2018-04-21 01:53:32 -0300

    test-consola actualizado

--- planificador/test/test-consola.c
+++ planificador/test/test-consola.c
@@ -1,14 +1,26 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 
+#include "../../libs/comando.h"
 #include "../consola.h"
+
+#define eprintf(args...) fprintf (stderr, args)
 
 int main(void)
 {
 	for (;;) {
 		char *line = readline("> ");
-		if (!execute_console_command(line)) {
-			printf("Comando invalido!\n");
+		int execute_result = execute_console_command(line);
+		switch (execute_result) {
+		case NO_COMMAND_ERROR:
+			eprintf("Comando invalido!\n");
+			break;
+		case ARGUMENT_COUNT_ERROR:
+			eprintf("Numero de argumentos incorrecto\n");
+			break;
+		default:
+			printf("Comando ejecutado correctamente\n");
+			break;
 		}
 		free(line);
 	}
