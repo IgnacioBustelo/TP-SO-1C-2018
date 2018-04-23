c78519321710b414c66ef0102cf9e9c8ef2567de
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-04-23 03:11:01 -0300

    Separo configuración de la instancia en un config.c

---
+++ instancia/config.h
@@ -0,0 +1,21 @@
+#ifndef INSTANCIA_CONFIG_H_
+#define INSTANCIA_CONFIG_H_
+
+typedef enum { CIRC, LRU, BSU } page_replacement_algorithm_t;
+
+typedef struct {
+	char* coordinator_ip;
+	int coordinator_port;
+	page_replacement_algorithm_t page_replacement_algorithm;
+	char* mount_point;
+	char* instance_name;
+	int dump_interval;
+} setup_t;
+
+t_log*	init_log();
+
+setup_t	init_config(t_log* logger);
+
+void handshake_coordinador(int coordinador_fd, t_log* logger);
+
+#endif
