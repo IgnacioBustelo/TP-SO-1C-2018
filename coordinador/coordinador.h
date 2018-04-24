d67132a90c59491cd7a16ccc7ac9d38a1a3cd2b3
Fernando <wzfernandochen@gmail.com>
2018-04-24 01:29:13 -0300

    Coordinador acepta la conexion del planificador primero

--- coordinador/coordinador.h
+++ coordinador/coordinador.h
@@ -3,6 +3,11 @@
 
 #include <commons/log.h>
 
+#define synchronized(lock)												\
+	for (pthread_mutex_t * i_##lock = &lock; i_##lock;					\
+		i_##lock = NULL, pthread_mutex_unlock(&lock))					\
+		for (pthread_mutex_lock(i_##lock); i_##lock; i_##lock = NULL)
+
 enum distribution_algorithm_t {
 	LSU, EL, KE
 };
