c2044040f3fd90da497a2330168c6f158041ae34
Fernando <wzfernandochen@gmail.com>
2018-04-25 05:16:20 -0300

    Algoritmo de Equitative Load (Sin testear).

---
+++ coordinador/distribution.c
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+
+#include "distribution.h"
+#include "instance-list.h"
+
+t_list *last_instances;
+
+static void last_instances_add(struct instance_t *elem);
+static bool list_contains(t_list *list, void *member);
+
+/* Executed before main() */
+__attribute__((constructor)) void init(void) {
+	last_instances = list_create();
+}
+
+/* Executed after main() */
+__attribute__((destructor)) void init(void) {
+	list_destroy(last_instances);
+}
+
+struct instance_t *equitative_load(t_list *instance_list)
+{
+	if (list_size(instance_list) >= list_size(last_instances)) {
+		list_remove(last_instances, 0);
+	}
+
+	bool last_instances_not_contains(void *elem) {
+		return !list_contains(last_instances, elem);
+	}
+
+	void *_next_instance = list_find(instance_list, last_instances_not_contains);
+	struct instance_t *next_instance = (struct instance_t *)_next_instance;
+	if (next_instance != NULL) {
+		last_instances_add(next_instance);
+	}
+
+	return next_instance;
+}
+
+static void last_instances_add(struct instance_t *elem)
+{
+	/* TODO: Mutexes */
+	list_add(last_instances, elem);
+	while (list_size(last_instances) >= list_size(instance_list)) {
+		list_remove(last_instances, 0);
+	}
+}
+
+static bool list_contains(t_list *list, void *member)
+{
+	bool equals(void *elem) {
+		return member == elem;
+	}
+	return list_any_satisfy(list, equals);
+}
---
+++ coordinador/distribution.h
@@ -0,0 +1,6 @@
+#ifndef COORDINADOR_DISTRIBUTION_H_
+#define COORDINADOR_DISTRIBUTION_H_
+
+extern t_list *last_instances;
+
+#endif /* COORDINADOR_DISTRIBUTION_H_ */
