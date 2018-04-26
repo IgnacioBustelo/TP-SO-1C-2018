3c493aefd51ac17d1fdfa3faa2228f8a1a7728bf
erikgervas <bernaderi@hotmail.com>
2018-04-26 20:22:49 -0300

    El ESI puede parsear línea por línea el archivo

--- esi/esi.c
+++ esi/esi.c
@@ -1,14 +1,16 @@
 #include "../libs/conector.h"
 #include "esi.h"
 #include "config.h"
+#include "../libs/serializador.h"
 
 /* -- Global variables -- */
 
 t_esi_config setup;
 
+FILE* fp;
 t_log* logger;
 
-int main(void) {
+int main(int argc, char **argv) {
 
 	logger = init_log();
 
@@ -43,7 +45,6 @@ int main(void) {
 			close(scheduler_fd);
 		}
 
-
 	received = receive_confirmation(scheduler_fd, &confirmation);
 		if (!received || !confirmation) {
 			log_error(logger, "Fallo en la confirmación de recepción de parte del planificador");
@@ -52,12 +53,64 @@ int main(void) {
 
     log_info(logger, "Conectado satisfactoriamente al planificador");
 
-	while(1) {
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
+    fp = fopen(argv[1], "r");
 
-	}
+    if (fp == NULL){
+
+        log_error(logger, "Fallo al abrir el archivo");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+
+    	package_t* package = obtain_package_from_line(line);
+
+    }
+
+    if (line) free(line);
 
 	exit_gracefully(EXIT_SUCCESS);
+}
+
+package_t obtain_package_from_line(char* line) {
+
+	package_t* package;
+    t_esi_operacion parsed = parse(line);
+
+    if(parsed.valido){
+
+        switch(parsed.keyword){
+
+            case GET:
+            	log_info(logger, "GET\tclave: <%s>", parsed.argumentos.GET.clave);
+            	/* TODO -- crear paquete para el caso de GET y los demás -- */
+                break;
+            case SET:
+            	log_info(logger, "SET\tclave: <%s>\tvalor: <%s>", parsed.argumentos.SET.clave, parsed.argumentos.SET.valor);
+                break;
+            case STORE:
+            	log_info(logger, "STORE\tclave: <%s>", parsed.argumentos.STORE.clave);
+                break;
+            default:
+            	log_error(logger, "No pude interpretar <%s>", line);
+            	free(line);
+                exit(EXIT_FAILURE);
+        }
+
+    } else {
+    	log_error(logger, "La linea <%s> no es valida", line);
+    	free(line);
+    	exit_gracefully(EXIT_FAILURE);
+    }
+
+    destruir_operacion(parsed);
+
+    return package;
+
 }
 
 void exit_gracefully(int status) {
@@ -65,6 +118,8 @@ void exit_gracefully(int status) {
 	log_info(logger, "La ejecución del ESI finalizó");
 
 	log_destroy(logger);
+
+    fclose(fp);
 
 	exit(status);
 }
--- esi/esi.h
+++ esi/esi.h
@@ -6,11 +6,18 @@
 #include <commons/string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <parsi/parser.h>
 
 /*
  * Se finaliza el programa satisfactoriamente o no
  */
 
 void exit_gracefully(int status);
+
+/*
+ * Obtiene el paquete que tiene que enviar al coordinador a partir de una línea del archivo
+ */
+
+package_t obtain_package_from_line(char* line);
 
 #endif
