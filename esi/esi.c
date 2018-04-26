e82346f859df1e91c84b906892ffdc46ce8d184b
erikgervas <bernaderi@hotmail.com>
2018-04-26 18:26:12 -0300

    Pongo logs del ESI en español

--- esi/esi.c
+++ esi/esi.c
@@ -14,8 +14,6 @@ int main(void) {
 
 	setup = init_config(logger);
 
-    //Creasignacion de las variables presentes en el archivo de configuracion
-
 	char* ip_coordinador = setup.ip_coordinador;
 	char* ip_planificador = setup.ip_planificador;
 	int port_coordinator = setup.port_coordinador;
@@ -23,49 +21,48 @@ int main(void) {
 
 	bool confirmation;
 
-    //Primera conexion, al coordinador:
-
 	int coordinator_fd = connect_to_server(ip_coordinador, port_coordinator);
 		if (send_handshake(coordinator_fd, ESI) != 1) {
-			log_error(logger, "Failure in send_handshake with coordinator");
+			log_error(logger, "Fallo en el envío del handshake al coordinador");
 			close(coordinator_fd);
 			exit_gracefully(EXIT_FAILURE);
 		}
 
 	int received = receive_confirmation(coordinator_fd, &confirmation);
 		if (!received || !confirmation) {
-			log_error(logger, "Failure in confirmation reception from coordinator");
+			log_error(logger, "Fallo en la confirmación de recepción de parte del coordinador");
 			close(coordinator_fd);
 			exit_gracefully(EXIT_FAILURE);
 		}
 
-	log_info(logger, "Succesfully connected to coordinator");
-
-    //Segunda conexion, al planificador:
+	log_info(logger, "Conectado satisfactoriamente al coordinador");
 
 	int scheduler_fd = connect_to_server(ip_planificador, port_scheduler);
 		if (send_handshake(scheduler_fd, ESI) != 1) {
-			log_error(logger, "Failure in send_handshake with scheduler");
+			log_error(logger, "Fallo en el envío del handshake al planificador");
 			close(scheduler_fd);
 		}
 
 
 	received = receive_confirmation(scheduler_fd, &confirmation);
 		if (!received || !confirmation) {
-			log_error(logger, "Failure in confirmation reception from scheduler");
+			log_error(logger, "Fallo en la confirmación de recepción de parte del planificador");
 			close(scheduler_fd);
 		}
 
-    log_info(logger, "Succesfully connected to scheduler");
+    log_info(logger, "Conectado satisfactoriamente al planificador");
 
-	while(1);
+	while(1) {
+
+
+	}
 
 	exit_gracefully(EXIT_SUCCESS);
 }
 
 void exit_gracefully(int status) {
 
-	log_info(logger, "ESI execution ended");
+	log_info(logger, "La ejecución del ESI finalizó");
 
 	log_destroy(logger);
 
