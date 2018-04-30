ac457a55dfa1931aed4f738ef198e4f4bb8d26a7
erikgervas <bernaderi@hotmail.com>
2018-04-30 00:06:57 -0300

    Arreglo una lepreadita

--- planificador/config.h
+++ planificador/config.h
@@ -19,7 +19,7 @@ typedef struct {
 	int port;
 	int coordinator_port;
 	t_scheduling_algorithm scheduling_algorithm;
-	float alpha;
+	double alpha;
 	double initial_estimation;
 	char* coordinator_ip;
 	char** blocked_keys;
