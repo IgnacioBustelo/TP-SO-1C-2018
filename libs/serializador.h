910dfef6ae192417bca6f17bbd0f3bbccad68f38
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-05-01 03:53:50 -0300

    Acomodo .h del serializador

--- libs/serializador.h
+++ libs/serializador.h
@@ -15,24 +15,26 @@ typedef struct {
 
 typedef enum { LOAD_SUCCESS, LOAD_MISSING, LOAD_EXTRA, SEND_SUCCESS, SEND_ERROR} package_status;
 
-package_t* create_package(size_t size);
+package_t*			create_package(size_t size);
 
-void add_content(package_t* package, void* content, size_t content_size);
+void				add_content(package_t* package, void* content, size_t content_size);
 
-void add_content_variable(package_t* package, void* content, size_t content_size);
+void				add_content_variable(package_t* package, void* content, size_t content_size);
 
-void add_content_list(package_t* package, t_list* list, size_t (*size_calculator)(void*));
+void				add_content_list(package_t* package, t_list* list, size_t (*size_calculator)(void*));
 
-package_status check_package(package_t* package);
+package_status		check_package(package_t* package);
 
-void* build_package(package_t* package);
+void* 				build_package(package_t* package);
 
-package_status send_serialized_package(int fd, void* serialized_package, size_t package_size);
+package_status		send_serialized_package(int fd, void* serialized_package, size_t package_size);
+
+void				destroy_package(package_t* package);
+
+char*				status_message(package_t* package, package_status status);
+
+// Deprecated
 
 package_t* receive_package(int socket_sender);
-
-void destroy_package(package_t* package);
-
-char* status_message(package_t* package, package_status status);
 
 #endif
