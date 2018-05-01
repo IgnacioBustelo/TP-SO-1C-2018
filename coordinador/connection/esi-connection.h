fed44865427adf8e74917a86ed5d30ee7009b7c4
Fernando <wzfernandochen@gmail.com>
2018-04-30 23:10:06 -0300

    Coordinador: Pequenio refactor.

--- coordinador/connection/esi-connection.h
+++ coordinador/connection/esi-connection.h
@@ -4,26 +4,6 @@
 #define CHECK_RECV_WITH_SIZE(FD, BLOCK, BLOCK_SIZE) (recv(FD, BLOCK, BLOCK_SIZE, MSG_WAITALL) != (BLOCK_SIZE))
 #define CHECK_RECV(FD, BLOCK) CHECK_RECV_WITH_SIZE(FD, BLOCK, sizeof(*(BLOCK)))
 
-enum operation_t {
-	ESI_GET, ESI_SET, ESI_STORE
-};
-
-struct esi_operation_t {
-	enum operation_t keyword;
-	union {
-		struct {
-			char* key;
-		} get;
-		struct {
-			char* key;
-			char* value;
-		} set;
-		struct {
-			char* key;
-		} store;
-	};
-};
-
 void handle_esi_connection(int fd);
 
 #endif /* COORDINADOR_CONNECTION_ESI_CONNECTION_H_ */
