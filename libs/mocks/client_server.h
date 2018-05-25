277eae6ee53ca9b85fb83d852a3a673ac6cb85db
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-05-24 22:04:06 -0300

    El test para probar cliente y servidor en 1 .c funciona bien

--- libs/mocks/client_server.h
+++ libs/mocks/client_server.h
@@ -1,23 +1,16 @@
 #ifndef LIBS_MOCKS_CLIENT_SERVER_H_
 #define LIBS_MOCKS_CLIENT_SERVER_H_
 
-#include <semaphore.h>
-
 #define HOST "127.0.0.1"
 #define PORT 8080
-
-sem_t server_sem;
-sem_t client_sem;
 
 char* server_name;
 char* client_name;
 
 void client_server_run();
 
-void client_server_show_semaphores();
+void client_server_execute_server(int fd_client);
 
 void client_server_execute_client(int fd_server);
-
-void client_server_execute_server(int fd_client);
 
 #endif
