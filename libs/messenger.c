52d21f89545325fb139a8e7d2a5b6944181d8843
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-06-08 22:35:49 -0300

    Corrijo el Warning de vasprintf

--- libs/messenger.c
+++ libs/messenger.c
@@ -1,10 +1,10 @@
+#define _GNU_SOURCE
+
 #include <commons/string.h>
 #include <string.h>
 #include <stdio.h>
 
 #include "messenger.h"
-
-#define _GNU_SOURCE
 
 void messenger_show(char* log_level, const char* format,...) {
 	va_list args;
