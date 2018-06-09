893ef55e2ac38249b0224cb5e1cc8fb02642dcb0
utnso <nicoxeneize.10@gmail.com>
2018-06-08 21:45:33 -0300

    Funciona algoritmo circular bien bien y devuelve lista de claves a reemplazar

--- instancia/tests/algorithms_test.c
+++ instancia/tests/algorithms_test.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-	storage_setup_init(10,20);
+	storage_setup_init(5,20);
 	entry_table_init();
 	key_value_t* key_value=key_value_generator("X",30);
 	int next_entry = entry_table_next_entry(key_value);
@@ -26,30 +26,26 @@ int main(void)
 	printf("\n");
 	entry_table_print_table(); //X,Y,Z
 
-	printf("Entries left: %d\n",entries_left);
-
 	key_value=key_value_generator("X",30);
 	entry_table_delete(key_value);
 
 	printf("\n");
 	entry_table_print_table(); //X,Y
 
-	printf("Entries left: %d\n",entries_left);
-
 	key_value=key_value_generator("A",15);
 	next_entry = entry_table_next_entry(key_value);
 	entry_table_insert(next_entry,key_value);
 
-	printf("\n");
 	entry_table_print_table(); //X,Y
 
+	printf("\n");
 	printf("Entries left: %d\n",entries_left);
 
 //	t_list* entry_table_status = original_entry_table_migration_to_complete_one();
 //
 //	entry_table_status_print_table(entry_table_status);
 
-	key_value=key_value_generator("T",30);
+	key_value=key_value_generator("A",15);
 
 	algorithm_circular_set_pointer(0);
 
@@ -57,27 +53,27 @@ int main(void)
 
 	algorithm_circular(entry_table,key_value,replaced_key);
 
-	printf("Aplico algoritmo circular Entries left: %d\n",entries_left);
+	printf("\n\n");
+
+	printf("Aplico algoritmo circular \n\n");
 
 	printf("\n\n");
 
-	for(int i=0;i<list_size(replaced_key);i++)
+	for(int i=0;i<list_size(entry_table);i++)
 	{
 		entry_table_delete(key_value_generator("A",15));
 	}
 
-	entry_table_print_table();
-
-//	key_value=key_value_generator("T",30);
-//
-//	next_entry = entry_table_next_entry(key_value);
-//	entry_table_insert(next_entry,key_value);
-//
-//
-//
-//	entry_table_print_table();
 
 
-	printf("Deberia guardar el nuevo kv Entries left: %d\n",entries_left);
+
+
+	key_value=key_value_generator("PP",30);
+	next_entry = entry_table_next_entry(key_value);
+	entry_table_insert(next_entry,key_value);
+
+		entry_table_print_table();
+
+	printf("Deberia guardar el nuevo kv Entries left: %d\n \n",entries_left);
 
 }
