5dc27709369c5f538dfb3a1f025289138fde10a9
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-06-08 22:26:33 -0300

    Cambio string_from_vformat por vasprintf

--- libs/messenger.c
+++ libs/messenger.c
@@ -1,14 +1,19 @@
 #include <commons/string.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "messenger.h"
+
+#define _GNU_SOURCE
 
 void messenger_show(char* log_level, const char* format,...) {
 	va_list args;
 
 	va_start(args, format);
 
-	char* message = string_from_vformat(format, args);
+	char* message;
+
+	vasprintf(&message, format, args);
 
 	messenger_show_method(log_level, message);
 
