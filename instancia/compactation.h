b1463345595c9d71e37dbdae9bb82eac1f443abd
utnso <nicoxeneize.10@gmail.com>
2018-06-18 01:27:42 -0300

    Agrego compactador

---
+++ instancia/compactation.h
@@ -0,0 +1,11 @@
+
+#ifndef INSTANCIA_COMPACTATION_H_
+#define INSTANCIA_COMPACTATION_H_
+
+#include <commons/collections/list.h>
+#include "entry_table.h"
+
+t_list* compact();
+
+
+#endif /* INSTANCIA_COMPACTATION_H_ */
