b4b50e89fda05723f3122f4aeb3a634a8b0e2dbb
Fernando <wzfernandochen@gmail.com>
2018-06-25 23:17:13 -0300

    Reasignar instancias por la desconexion de una.

--- coordinador/instance-list/instance-request-list.h
+++ coordinador/instance-list/instance-request-list.h
@@ -33,7 +33,9 @@ struct request_list_t *request_list_create(void);
 void request_list_destroy(struct request_list_t *list);
 struct request_node_t *request_list_push_set(struct request_list_t *request_list, int esi_fd, char *key, char *value);
 struct request_node_t *request_list_push_store(struct request_list_t *request_list, int esi_fd, char *key);
+void request_list_push(struct request_list_t *request_list, struct request_node_t *elem);
 struct request_node_t *request_list_pop(struct request_list_t *request_list);
+void request_list_iterate(struct request_list_t *request_list, void (*closure)(void *));
 void request_node_destroy(struct request_node_t *node);
 
 #endif /* COORDINADOR_INSTANCE_REQUEST_LIST_H_ */
