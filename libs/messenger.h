4487c4f4c8d65f27e706aef53b771d09beae5a7f
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-07-07 23:57:03 -0300

    Añado funcion al messenger para extraer un csv de una lista de strings

--- libs/messenger.h
+++ libs/messenger.h
@@ -1,6 +1,7 @@
 #ifndef LIBS_MESSENGERS_MESSENGER_H_
 #define LIBS_MESSENGERS_MESSENGER_H_
 
+#include <commons/collections/list.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
@@ -11,6 +12,8 @@ void	messenger_show(char* log_level, const char* format,...);
 void	messenger_show_method(char* log_level, char* message);
 
 char*	messenger_bytes_to_string(void* data, size_t size);
+
+char*	messenger_list_to_string(t_list* string_list);
 
 int		messenger_longest_string_length(int max_number);
 
