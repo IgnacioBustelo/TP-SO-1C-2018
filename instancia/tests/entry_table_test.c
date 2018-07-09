25a9b84bf8de322e14e58ee61407b1d4747247a4
utnso <nicoxeneize.10@gmail.com>
2018-07-09 19:46:17 -0300

    Cambios

--- instancia/tests/entry_table_test.c
+++ instancia/tests/entry_table_test.c
@@ -8,30 +8,30 @@
 int main()
 {
 
-		storage_setup_init(10,20);
+		storage_setup_init(5,20);
 		entry_table_init();
-		key_value_t* key_value=key_value_generator("X",30);
+		key_value_t* key_value=key_value_generator("X",2);
 		int next_entry = entry_table_next_entry(key_value);
 		entry_table_insert(next_entry,key_value);
 
-		key_value=key_value_generator("Y",30);
-		next_entry = entry_table_next_entry(key_value);
-		entry_table_insert(next_entry,key_value);
+//		key_value=key_value_generator("Y",30);
+//		next_entry = entry_table_next_entry(key_value);
+//		entry_table_insert(next_entry,key_value);
+//
+//		key_value=key_value_generator("Z",15);
+//		next_entry = entry_table_next_entry(key_value);
+//		entry_table_insert(next_entry,key_value);
 
-		key_value=key_value_generator("Z",15);
-		next_entry = entry_table_next_entry(key_value);
-		entry_table_insert(next_entry,key_value);
+//		printf("\n");
+//		entry_table_print_table(); //X,Y,Z
+//
+//		key_value=key_value_generator("X",30);
+//		entry_table_delete(key_value);
+//
+//		printf("\n");
+//		entry_table_print_table(); //X,Y
 
-		printf("\n");
-		entry_table_print_table(); //X,Y,Z
-
-		key_value=key_value_generator("X",30);
-		entry_table_delete(key_value);
-
-		printf("\n");
-		entry_table_print_table(); //X,Y
-
-		key_value=key_value_generator("A",15);
+		key_value=key_value_generator("X",65);
 		next_entry = entry_table_next_entry(key_value);
 		entry_table_insert(next_entry,key_value);
 
